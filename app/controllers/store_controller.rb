ecee218ab7f52b865c0da65e6469b4994c7d6e20
Renato Ramiro <renato.ipj@gmail.com>
2012-09-01 10:57:37 -0300

    Playtime chapter 9

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,17 @@ class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
   	@time = Time.now.strftime("%d/%m/%Y %H:%M:%S")
+  	@message = show_message
   end
+
+private
+  def count_sessions
+		session[:counter] ||= 0
+		session[:counter] += 1
+	end
+
+	def show_message
+		counter = count_sessions
+		@message = "This page was accessed #{counter} times." if counter > 5
+	end
 end
