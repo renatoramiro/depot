85b03143ee174468c485a2d7d19c59dcf6a42e47
Renato Ramiro <renato.ipj@gmail.com>
2012-11-05 16:29:55 -0300

    Playtime Chapter 12

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -84,6 +84,8 @@ class ProductsController < ApplicationController
   def who_bought
     @product = Product.find(params[:id])
     respond_to do |format|
+      format.xml { render xml: @product.to_xml(include: :orders) }
+      format.json {render :json => @product.to_json(:include => :orders)}
       format.atom
     end
   end
